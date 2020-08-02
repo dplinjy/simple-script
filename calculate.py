1d0cc796a533cdb4a977d680430f1fa0585024cf
dplinjy <dplinjy@163.com>
2020-08-02 15:55:10 +0800

    first commit

---
+++ calculate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+class Calculation(object):
+    @staticmethod
+    def add(a, b):
+        return a+b
+    @staticmethod
+    def minus(a, b):
+        return a-b
+    @staticmethod
+    def multiple(a, b):
+        return a*b
+    @staticmethod
+    def divide(a, b):
+        return a/b
---
+++ testCalculate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import unittest
+from calculate import Calculation
+
+class TestCalculation(unittest.TestCase):
+    def test_add(self):
+        a = 1
+        b = 2
+        result = Calculation.add(a, b)
+        assert result == 3
+
+    def test_minus(self):
+        a = 6
+        b = 2
+        result = Calculation.minus(a, b)
+        assert result == 4
+
+    def test_multiply(self):
+        a = 3
+        b = 4
+        result = Calculation.multiple(a, b)
+        assert result == 12
+
+    def test_divide(self):
+        a = 30
+        b = 5
+        result = Calculation.divide(a, b)
+        assert result == 6
+
+
+if __name__ == '__main__':
+    unittest.main()
